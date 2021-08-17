c4a00a0108ca158d3814b7341ffffe811b4a7752
kakumanus <sujankakumanu@gmail.com>
2021-08-17 14:53:02 -0400

    Additions to Project Pages
    Continued buildout of CRPLAN Page, consolidation of Hack OHI/O pages, smaller edits.

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,1 +1,10 @@
 // This is where it all goes :)
+function confirmation() {
+	var answer = confirm("For academic integrity reasons, access to the FEH website requires a password. Please contact me for this password.")
+	if (answer){
+		window.open("https://u.osu.edu/feh18e8/");
+	}
+	else{
+
+	}
+}
